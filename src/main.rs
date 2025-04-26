61c956d1c5ed666d4dccd39cfbcddb49d72c6ccc
Harsh  Agarwal <55704229+harshagarwal2245@users.noreply.github.com>
2025-04-26 15:11:37 +0530

    Update main.rs
    Update documentation

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-        
+    // a random number to guess     
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
-
+    // take user input
     let mut guess = String::new();
 
     io::stdin()
@@ -16,9 +16,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
-    
+    // typecast string to int
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
+
+    //Update user about there attempt
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
